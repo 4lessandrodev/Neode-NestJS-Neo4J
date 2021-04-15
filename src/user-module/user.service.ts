d1ed839f543b539c665e3b5336e07dc50a56969f
Alessandrodev <alessandroadm@live.com>
2021-04-15 16:49:29 -0300

    feat(user.service): add service to user module

---
+++ src/user-module/user.service.ts
@@ -0,0 +1,30 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { LoggerService } from '../logger-module/logger.service';
+import Neode from 'neode';
+import { UserInterface } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject(LoggerService) private readonly logger: LoggerService,
+    @Inject('Connection') private readonly neode: Neode,
+  ) {}
+
+  async createUser(dto: CreateUserDto): Promise<void> {
+    this.logger.log('Create User');
+    dto.id = uuid();
+    dto.isFirstAuth = true;
+    console.log(this.neode);
+
+    await this.neode.merge('User', dto);
+    this.logger.log('User has been created');
+  }
+
+  async getUsers(): Promise<UserInterface[]> {
+    this.logger.log('Get Users');
+    const user = await this.neode.all('User');
+    return (await user.toJson()) as UserInterface[];
+  }
+}
