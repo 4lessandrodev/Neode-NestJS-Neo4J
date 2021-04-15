615b996219a869915971f1ee68388b3d49a72bf1
Alessandrodev <alessandroadm@live.com>
2021-04-15 16:48:36 -0300

    feat(neode-module): add error message as log and inject schema on feature method

--- src/neode-module/neode.module.ts
+++ src/neode-module/neode.module.ts
@@ -1,9 +1,18 @@
-import { Global, Module, DynamicModule } from '@nestjs/common';
+import { Global, Module, DynamicModule, Logger } from '@nestjs/common';
 import * as Neode from 'neode';
+
+interface Config {
+  [label: string]: Neode.SchemaObject;
+}
 
 @Global()
 @Module({})
 export class NeodeModule {
+  /**
+   *
+   * @param directory schemas folder as path example: `src/infra/schemas`
+   * @returns instance of Neode as connection
+   */
   static forRoot(directory?: string): DynamicModule {
     return {
       module: NeodeModule,
@@ -14,44 +23,69 @@ export class NeodeModule {
           useValue: directory ?? null,
         },
         {
-          provide: Neode,
+          provide: 'Connection',
           useFactory: async () => {
             let connection: Neode;
             if (typeof directory === 'string') {
               connection = await Neode.fromEnv().withDirectory(directory);
-              await connection.schema.install();
+
+              // Why error if already installed?
+              try {
+                await connection.schema.install();
+              } catch (error) {
+                new Logger('NeodeModule', true).warn(
+                  `Could not install schemas from directory ${directory}. Already installed?`,
+                );
+              }
             } else {
               connection = await Neode.fromEnv();
             }
+
             return connection;
           },
           inject: ['DIRECTORY'],
         },
       ],
-      exports: [Neode],
+      exports: ['Connection'],
     };
   }
 
-  static forFeature(schema: Neode.SchemaObject): DynamicModule {
+  /**
+   *
+   * @param config key: as label name and value: Node definition as `SchemaObject`
+   * example: `{ User: UserSchema }`
+   * @returns Instance of Neode as connection for module
+   */
+  static forFeature(config: Config): DynamicModule {
     return {
       module: NeodeModule,
       global: false,
       providers: [
         {
-          provide: 'SCHEMA',
-          useValue: schema,
+          provide: 'CONFIG',
+          useValue: config,
         },
         {
-          provide: Neode,
+          provide: 'Connection',
           useFactory: async () => {
-            const connection = await Neode.fromEnv().with({ schema });
-            await connection.schema.install();
+            const connection = await Neode.fromEnv().with(config);
+
+            // Why error if already installed?
+            try {
+              await connection.schema.install();
+            } catch (error) {
+              new Logger('NeodeModule', true).error(
+                `Could not install schema ${Object.keys(
+                  config,
+                )}. Already installed ?`,
+              );
+            }
             return connection;
           },
-          inject: ['SCHEMA'],
+          inject: ['CONFIG'],
         },
       ],
-      exports: [Neode],
+      exports: ['Connection'],
     };
   }
 }
