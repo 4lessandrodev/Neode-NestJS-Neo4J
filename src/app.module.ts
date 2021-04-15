696d851dba1a154f0064884d45dd130d2ccdf1d0
Alessandrodev <alessandroadm@live.com>
2021-04-15 16:51:33 -0300

    feat(app.module): inject user.module on root app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { LoggerModule } from './logger-module/logger.module';
 import { NeodeModule } from './neode-module/neode.module';
+import { UserModule } from './user-module/user.module';
 
 @Module({
-  imports: [LoggerModule.forRoot('Root'), NeodeModule.forRoot()],
+  imports: [LoggerModule.forRoot('Root'), NeodeModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [],
 })
